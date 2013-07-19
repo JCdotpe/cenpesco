230e7aa07379bc72202f49f69a77ace53a3e5998
jhonatanrivera <yonatan.rc@gmail.com>
2013-07-19 18:20:29 -0500

    reporte segmentaicon
    ffdff

---
+++ application/models/segmentacion_model.php
@@ -0,0 +1,92 @@
+<?php 
+
+class Segmentacion_model extends CI_MODEL {
+
+
+	 public function get_empadronador($sede, $dep, $equi, $ruta)
+	{
+		
+		$q = $this->db->query("select  DISTINCT(SEDE_OPERATIVA),  DEPARTAMENTO_SEDE,  EQUIPO,  RUTA,  NOMBRE_Y_APELLIDOS_DEL_EMPADRONADOR from data_empadronador  
+		where sede_operativa = (select  distinct(nom_sede) from marco where sede_cod = '". $sede ."') 
+		and departamento_sede = (select  distinct(departamento) from marco where ccdd = '". $dep ."') 
+		and equipo = ". $equi ."
+		and ruta = ". $ruta);
+		return $q;
+
+	}
+
+	 public function get_all_empadronador($sede, $dep, $equi, $ruta)
+	{
+		
+		$q = $this->db->query("select 
+			  N,
+			  DEPARTAMENTO,
+			  PROVINCIA,
+			  DISTRITO,
+			  CENTRO_POBLADO,
+			  POBLACION_ESTIMADA,
+			  TIPO_EMPADRONAMIENTO,
+			  FECHA_INICIO,
+			  FECHA_FINAL,
+			  TRASLADO,
+			  TRABAJO,
+			  VIAJE_SEDE_ODEI,
+			  RETROALIMENTACION,
+			  TOTAL_DE_DIAS,
+			  MOV_LOCAL decim,
+			  PASAJE_TRASLADO,
+			  GASTO_OPERATIVO,
+			  MOVILIDAD_LOCAL_S,
+			  PASAJE_TRASLADO_2,
+			  GASTOS_OPERATIVO_S,
+			  PASAJE_S,
+			  TOTAL_S,
+			  PUNTO_CONCENTRACION,
+			  CCPP_CV1,
+			  CCPP_CV2,
+			  CCPP_CV3,
+			  CCPP_CV4,
+			  CCPP_CV5,
+			  CCPP_CV6,
+			  CCPP_CV7,
+			  CCPP_CV8,
+			  CCPP_CV9,
+			  CCPP_CV10,
+			  CCPP_CV11,
+			  CCPP_CV12,
+			  CCPP_CV13,
+			  CCPP_CV14,
+			  CCPP_CV15,
+			  CCPP_CV16,
+			  CCPP_CV17,
+			  CCPP_CV18,
+			  CCPP_CV19,
+			  CCPP_CV20,
+			  CCPP_CV21,
+			  CCPP_CV22,
+			  CCPP_CV23
+			from data_empadronador  
+			where sede_operativa = (select  distinct(nom_sede) from marco where sede_cod = '". $sede ."') 
+			and departamento_sede = (select  distinct(departamento) from marco where ccdd = '". $dep ."') 
+			and equipo = ". $equi ."
+			and ruta = ". $ruta);
+		return $q;
+
+
+ 
+
+
+
+
+	}
+
+
+
+
+
+
+
+
+
+
+}
